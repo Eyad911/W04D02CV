6a8acfa50f9c1d5aaf8e42f52a2623e50577d895
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-11-01 17:02:29 +0300

    done

---
+++ cv/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Info from './components/Info';
+import Experience from './components/Experience';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <Experience/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cv/src/components/Experience/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+
+const Experience = ()=> {
+   
+        return (
+            <div className="experDiv">
+                <div><h1 className="aboutH1">ABOUT</h1><hr/><p>Over the past 20 years I have continuously challenged myself with new languages, frameworks and methodologies. My core languages have always been html, css & javascript but I have also worked as a full stack php developer building both bespoke CMS solutions and small business applications. My current role at IAG is Senior Front End Developer (or Senior Software Engineer as IAG prefer to label us) and have been developing with React and Redux for the past 4 years.</p> <br/> </div>
+                <div><h1 className="workEXH1">WORK EXPERIENCE</h1><hr/> <h3>Instructor at the Golden Course</h3>  <h6>11/2020-08/2021</h6> <p>I was working as Instructor at the Golden Course , Iwork for 11 months with golden course,I was working as Instructor at the Golden Course , Iwork for 11 months with golden course.</p></div>
+                <div><h1 className="skillsH1">SKILLS</h1><hr/><div className="parDivBK"> <p>HTML</p><p>CSS</p><p>JS</p><p>C#</p><p>SWIFT</p><p>PHP</p><p>GIT</p><p>ES6</p><p>HTML</p><p>HTML</p><p>Heroku</p> </div></div>
+                <div><h1 className="eduH1">EDUCATION</h1><hr/> <h3>Qassim University ,College of Computer</h3> <h6>Studied:04/2012-01/2018</h6></div>
+            </div>
+        )
+    
+}
+export default Experience
